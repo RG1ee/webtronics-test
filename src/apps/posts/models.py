d8399aeffa0f41859a97daa5143388774d134528
RG1ee <niwows@yandex.ru>
2023-08-01 16:54:35 +0300

    create post table and create migration

---
+++ src/apps/posts/models.py
@@ -0,0 +1,18 @@
+import datetime
+
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from src.database.db import Base
+
+
+class Post(Base):
+    __tablename__ = "post"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    post_name = Column(String(256), nullable=False)
+    content = Column(String(500), nullable=False)
+    date_created = Column(DateTime, default=datetime.datetime.utcnow)
+
+    user = relationship("User", back_populates="posts")
